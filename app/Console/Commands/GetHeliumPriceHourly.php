db8a1a39d7c03a65e19a7bec7b18f46a65a5176a
JeffNicholls97 <36538244+JeffNicholls97@users.noreply.github.com>
2022-10-07 13:31:14 +0100

    crons removed log

--- app/Console/Commands/GetHeliumPriceHourly.php
+++ app/Console/Commands/GetHeliumPriceHourly.php
@@ -40,8 +40,6 @@ class GetHeliumPriceHourly extends Command
      */
     public function handle()
     {
-        \Log::info("Cron is working fine!");
-
         $coinResponse = Http::withHeaders([
             'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
         ])->get('https://api.coingecko.com/api/v3/simple/price?ids=helium&vs_currencies=gbp');
